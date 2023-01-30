37033869d8fd838c8b20091b4bf0efc837dbce8b
Lee Spitzley <lspitzley@albany.edu>
2023-01-30 12:38:02 -0500

    bash scripts from 2-2 and 3-1

---
+++ args_lab.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# this script will demonstrate arguments
+
+echo "The script has started"
+
+echo $#   # shows the number of arguments that were passed in
+echo $1   # shows the first argument (if it exists)
+echo $2   # shows the second argument (if it exists)
+echo $3   # ...
+
+
+echo "The script has finished."
---
+++ echo_lab.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+################################################
+# This script will print out your name and the #
+# contents of your home directory              #
+################################################
+
+# print the contents of the current directory
+echo $(ls)
