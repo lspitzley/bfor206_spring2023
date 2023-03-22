4cf6bc711f806198827327d034893d11b44a1fee
Lee Spitzley <lspitzley@albany.edu>
2023-03-22 13:03:36 -0400

    add posts to dataframe

--- get_reddit_posts.py
+++ get_reddit_posts.py
@@ -7,6 +7,7 @@ This script will use praw to get posts from reddit.
 import os
 import praw
 from dotenv import load_dotenv
+import pandas as pd
 
 #%% load credentials
 # store API credntials
@@ -28,5 +29,31 @@ submissions = reddit.subreddit("test").hot(limit=10)
 for submission in submissions:
 	print(submission.title)
 
+
+# %% get posts from AskReddit and put them in a dataframe
+
+submissions = reddit.subreddit("AskReddit").hot(limit=100)
+
+# use a for loop to store the posts in a list
+
+# create an empty list
+df_rows = []
+
+# for loop over the submissions
+for submission in submissions:
+	
+    # add list to the master list
+	df_rows.append([submission.id, submission.score, submission.title])
+
+
+# %% show the data
+
+# make sure to add import pandas as pd at the top of the script
+
+post_df = pd.DataFrame(df_rows, columns=['id', 'score', 'title'])
+
+# %% histogram of post scores
+
+post_df['score'].plot.hist()
 
 # %%
