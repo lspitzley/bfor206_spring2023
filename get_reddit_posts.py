89dc93dc6b931a5959e1d04de31934761e958d9a
Lee Spitzley <lspitzley@albany.edu>
2023-03-27 12:51:34 -0400

    refactor code to use functions
    also added function to get comments as a dataframe

--- get_reddit_posts.py
+++ get_reddit_posts.py
@@ -18,42 +18,61 @@ print('APP_ID:\t\t', APP_ID)
 print('API_SECRET:\t', API_SECRET)
 
 # %% connect to reddit
-reddit = praw.Reddit(client_id=APP_ID,
-	client_secret=API_SECRET,
-	user_agent='Post Downloader')
-print('Successful app authentication:', reddit.read_only)
+def reddit_connection():
+	reddit = praw.Reddit(client_id=APP_ID,
+		client_secret=API_SECRET,
+		user_agent='Post Downloader')
+	print('Successful app authentication:', reddit.read_only)
 
+	return reddit
 
-# %%  Use PRAW to download posts
-submissions = reddit.subreddit("test").hot(limit=10)
-for submission in submissions:
-	print(submission.title)
+# TypeHints (-> ) tell you what should be returned 
+def get_submissions(reddit, subreddit, limit=10) -> pd.DataFrame:
+	"""
+	This function will get posts from a subreddit
+	and return them as a dataframe.
+	"""
 
+	submissions = reddit.subreddit(subreddit).hot(limit=limit)
 
-# %% get posts from AskReddit and put them in a dataframe
-
-submissions = reddit.subreddit("AskReddit").hot(limit=100)
-
-# use a for loop to store the posts in a list
-
-# create an empty list
-df_rows = []
-
-# for loop over the submissions
-for submission in submissions:
-	
     # add list to the master list
-	df_rows.append([submission.id, submission.score, submission.title])
+	df_rows = []
+	for submission in submissions:
+		df_rows.append([submission.id, submission.score, submission.title, submission.num_comments])
 
+	post_df = pd.DataFrame(df_rows, columns=['id', 'score', 'title', 'num_comments'])
 
-# %% show the data
+	return post_df
 
-# make sure to add import pandas as pd at the top of the script
+def get_comments_from_post(reddit, post_id) -> pd.DataFrame:
+	"""
+	This function will return a dataframe of comments for a 
+	post. 
+	"""
+	# get the submission
+	submission = reddit.submission(id=post_id)
 
-post_df = pd.DataFrame(df_rows, columns=['id', 'score', 'title'])
+	# get a list of the comments
+	submission.comments.replace_more(limit=0)
+	comments_list = submission.comments.list()
 
-# %% histogram of post scores
+	comment_rows = []
+	for comment in comments_list:
+		comment_rows.append([comment.id, comment.score, comment.created_utc, comment.body, comment.parent_id])
 
-post_df['score'].plot.hist()
+	comments_df = pd.DataFrame(comment_rows, columns=['id', 'score', 'created_utc', 'body', 'parent_id'])
+
+	return comments_df
+
+# %% test our functions
+
+# authentication function
+reddit = reddit_connection()
+
+# check the get submissions function
+submission_df = get_submissions(reddit, 'cybersecurity')
+
+# check the comments dataframe function
+comments_df = get_comments_from_post(reddit, post_id='120j74c')
 
 # %%
